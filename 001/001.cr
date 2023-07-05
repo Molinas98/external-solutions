be6e65d8c1f12a2ce07d39d900b84bffe1e0a69e
tadeomolinas <tade25.molinas@gmail.com>
2023-07-05 16:47:05 -0400

    add nuevos mensajes

---
+++ 001/001.cr
@@ -0,0 +1,6 @@
+def sumar(num1, num2)
+  return num1 + num2
+end
+
+puts "#{sumar(86, 4)}"
+
---
+++ 001/001.scm
@@ -0,0 +1,5 @@
+; Función para sumar dos números
+(define (sumar a b)
+  (+ a b))
+(display (sumar 17 33))
+(newline)
